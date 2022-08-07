da5889c8eb87bd993bfaa65dca8cc79a500bc645
Valeria Mocrousova <Valeriabogoslavets@tandex.ru>
2022-08-07 20:02:35 +0300

    feat: исправление неточностей

--- script/script.js
+++ script/script.js
@@ -1,30 +1,30 @@
-let profileEditButton = document.querySelector('.profile__edit-button');
+let profileEditButton = document.querySelector('.profile__edit-btn');
 let profileName = document.querySelector('.profile__name');
 let profileStatus = document.querySelector('.profile__status');
 let popup = document.querySelector('.popup');
 let popupInputUserName = document.querySelector('.popup__input_username');
 let popupAboutMe = document.querySelector('.popup__input_aboutme');
-let popupButtonClose = document.querySelector('.popup__button-close');
-let popupForm = document.querySelector('.popup__form');
 
 function popupOpened() {
     popupInputUserName.value = profileName.textContent;
     popupAboutMe.value = profileStatus.textContent;
     popup.classList.add('popup__opened')
 }
+profileEditButton.addEventListener('click', popupOpened);
 
-profileEditButton.addEventListener('click',popupOpened);
 
+let popupButtonClose = document.querySelector('.popup__button-close');
 function popupClose() {
     popup.classList.remove('popup__opened');
 }
+popupButtonClose.addEventListener('click', popupClose);
 
-
-function formSubmitHandler (evt) {
+let popupForm = document.querySelector('.popup__form');
+function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = popupInputUserName.value;
     profileStatus.textContent = popupAboutMe;
     popupClose()
 }
-popupButtonClose.addEventListener('click', popupClose); 
+
 popupForm.addEventListener('submit', formSubmitHandler);
